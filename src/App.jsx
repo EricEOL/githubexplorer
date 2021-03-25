24c3b04a56740b7f240b15c3bc9a63635b081cb3
EricEOL <lopes.eric051@gmail.com>
2021-03-25 19:35:52 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './styles/global.scss';
+
+export function App() {
+    return (
+        <div>
+            <h1>Rocketseat - Ignite</h1>
+            <p>1º Projeto</p>
+        </div>
+    )
+}
