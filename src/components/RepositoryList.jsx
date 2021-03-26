825aa279e4e402ccb6181b67d74f4fe90f0e02d7
EricEOL <lopes.eric051@gmail.com>
2021-03-26 17:07:40 -0300

    Chamada a API do GitHub e renderizando repositórios em tela

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,28 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
-
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/EricEOL/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem />
+                {repositories && repositories.map(repository => (
+                    <RepositoryItem
+                        key={repository.id}
+                        repository={repository}
+                    />
+                ))}
             </ul>
         </section>
     );
